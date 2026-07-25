96863475fb6c5362d9df64e1ee1209c582118d8c
Alberto Tudela <ajtudela@gmail.com>
2026-07-25 21:30:23 +0200

    ci: re-enable linters and tests, fix the lint errors they surface
    * Remove `skip-tests: true` from the build workflow so `ament_flake8`,
      `ament_pep257`, `ament_copyright` and the pytest suite actually run;
      until now the build badge passed without checking any of them.
    * Fix the flake8 line-length and trailing-whitespace violations in
      setup.py that this re-enables, which were previously hidden by the
      skipped tests.

--- setup.py
+++ setup.py
@@ -8,26 +8,29 @@ metric_name = 'metric_depth'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        # Install Python submodule files Default DepthAnything V2 
+        # Install Python submodule files Default DepthAnything V2
         (os.path.join('lib', package_name, submodule_name, submodule_name),
          glob(os.path.join(submodule_name, submodule_name, '*.py'))),
         (os.path.join('lib', package_name, submodule_name, submodule_name, 'dinov2_layers'),
          glob(os.path.join(submodule_name, submodule_name, 'dinov2_layers', '*.py'))),
         (os.path.join('lib', package_name, submodule_name, submodule_name, 'util'),
          glob(os.path.join(submodule_name, submodule_name, 'util', '*.py'))),
-        # Install Python submodule files Metric DepthAnything V2 
+        # Install Python submodule files Metric DepthAnything V2
         (os.path.join('lib', package_name, submodule_name, metric_name, submodule_name),
          glob(os.path.join(submodule_name, metric_name, submodule_name, '*.py'))),
-        (os.path.join('lib', package_name, submodule_name, metric_name, submodule_name, 'dinov2_layers'),
-         glob(os.path.join(submodule_name, metric_name, submodule_name, 'dinov2_layers', '*.py'))),
+        (os.path.join(
+            'lib', package_name, submodule_name, metric_name, submodule_name, 'dinov2_layers'),
+         glob(os.path.join(
+             submodule_name, metric_name, submodule_name, 'dinov2_layers', '*.py'))),
         (os.path.join('lib', package_name, submodule_name, metric_name, submodule_name, 'util'),
-         glob(os.path.join(submodule_name, metric_name, submodule_name, 'util', '*.py'))), 
+         glob(os.path.join(submodule_name, metric_name, submodule_name, 'util', '*.py'))),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
         (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.pth'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml')))
     ],
